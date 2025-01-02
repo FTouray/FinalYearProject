891cf8e4f31156a9f5b67dcc28f207381775fbd8
FTouray <ftouray9@gmail.com>
2025-01-02 11:34:08 +0000

    Url

--- glycolog/core/urls.py
+++ glycolog/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (meal_step, glucose_step, glycaemic_response_main, insights_graph_data, list_categories, list_food_items_by_category, log_glucose, log_meal, meal_log_detail,  meal_log_history, register_user, 
+from .views import (exercise_step, meal_step, glucose_step, glycaemic_response_main, insights_graph_data, list_categories, list_food_items_by_category, log_glucose, log_meal, meal_log_detail,  meal_log_history, register_user, 
     login_user, settings_view, glucose_log_details, glucose_log_history, start_questionnaire, symptom_step)
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
@@ -30,5 +30,6 @@ urlpatterns = [
     path("questionnaire/symptom-step/", symptom_step, name="symptom-step"),  # Handle symptom step
     path("questionnaire/glucose-step/", glucose_step, name="glucose-step"),  # Handle glucose step
     path("questionnaire/meal-step/", meal_step, name="meal-step"),  # Handle diet step
+    path("questionnaire/exercise-step/", exercise_step, name="exercise-step"),
     path("insights-graph/", insights_graph_data, name="insights-graph-data"),  # Get insights
 ]
