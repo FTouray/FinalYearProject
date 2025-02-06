62bc19398f105034823a51d1482f5aea8b2389b3
FTouray <ftouray9@gmail.com>
2025-02-06 23:54:40 +0000

    Add settings.py and update .gitignore to exclude .env

--- glycolog/glycolog/settings.py
+++ glycolog/glycolog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+-+xxnmmnagi8+_5!%wmil!n-l%^^g9)wa)cy^+g#ig3$+8h$6'
+SECRET_KEY = os.getenv("SECRET_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
